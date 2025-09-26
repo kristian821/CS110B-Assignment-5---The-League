8362e7d84225e0f8ed871562d31e632f02a8b5df
Kristian Mitchell <56667566+kristian821@users.noreply.github.com>
2025-09-25 19:43:07 -0700

    initial commit

---
+++ Assignment 5 - The League/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment 5 - The League
+//
+//  Created by Kristian Mitchell on 9/25/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
